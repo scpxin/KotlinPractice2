5a825f24728bd1861d772abe775d90578f2af4c7
scpxin <2137459298@qq.com>
2020-07-09 23:21:01 +0800

    底部菜单栏完成

--- app/src/main/java/com/example/kotlinpractice/Fragment/Login/LoginFragment.kt
+++ app/src/main/java/com/example/kotlinpractice/Fragment/Login/LoginFragment.kt
@@ -1,14 +1,14 @@
 package com.example.kotlinpractice.Fragment.Login
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.example.kotlinpractice.R
-import com.example.kotlinpractice.databinding.ActivityLoginBinding
-import com.example.kotlinpractice.databinding.LoginFragmentBinding
+
 
 class LoginFragment : Fragment() {
 
@@ -17,13 +17,13 @@ class LoginFragment : Fragment() {
     }
 
     private lateinit var viewModel: LoginViewModel
-    private lateinit var binding: LoginFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = LoginFragmentBinding.inflate(layoutInflater)
-        return binding.root
+
+        return inflater.inflate(R.layout.login_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -34,8 +34,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.loginBtnLogin.setText("ddd")
-        binding.loginMobile.setText("dd")
+
     }
 
 }
--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.message_icon_layout.*
+import kotlinx.android.synthetic.main.museum_icon_layout.*
+import kotlinx.android.synthetic.main.person_icon_layout.*
+import kotlinx.android.synthetic.main.search_icon_layout.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.fragment)
+
+        val destinationMap = mapOf(
+            R.id.messageFragment to messageMotionlayout,
+            R.id.museumFragment to museumMotionlayout,
+            R.id.searchFragment to searchMotionlayout,
+            R.id.personFragment to personMotionlayout
+        )
+
+//        messageMotionlayout.setOnClickListener { navController.navigate(R.id.messageFragment) }
+//        museumMotionlayout.setOnClickListener { navController.navigate(R.id.museumFragment) }
+//        searchMotionlayout.setOnClickListener { navController.navigate(R.id.searchFragment) }
+//        personMotionlayout.setOnClickListener { navController.navigate(R.id.personFragment) }
+        destinationMap.forEach{map->
+            map.value.setOnClickListener { navController.navigate(map.key) }
+
+        }
+
+        setupActionBarWithNavController(
+            navController,
+            //AppBarConfiguration(setOf(R.id.messageFragment,R.id.museumFragment,R.id.searchFragment,R.id.personFragment))
+            AppBarConfiguration(destinationMap.keys)
+        )
+
+        navController.addOnDestinationChangedListener{controller, destination, arguments ->
+            controller.popBackStack()
+//            messageMotionlayout.progress = 0f
+//            museumMotionlayout.progress = 0f
+//            searchMotionlayout.progress = 0f
+//            personMotionlayout.progress = 0f
+            destinationMap.values.forEach{it.progress = 0f}
+            destinationMap[destination.id]?.transitionToEnd()
+//            when(destination.id) {
+////                R.id.messageFragment -> messageMotionlayout.transitionToEnd()
+////                R.id.museumFragment -> museumMotionlayout.transitionToEnd()
+////                R.id.searchFragment -> searchMotionlayout.transitionToEnd()
+////                else -> personMotionlayout.transitionToEnd()
+////            }
+
+        }
     }
 }
